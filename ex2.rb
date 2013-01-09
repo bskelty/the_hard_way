0d0aaccc2d22f8800558ed6e5b006a15b217b007
bskelty <brad@cheapcycleparts.com>
2013-01-09 10:32:28 -0500

    Changes

--- ex2.rb
+++ ex2.rb
@@ -6,5 +6,5 @@ puts "I could have code like this." # And the comment after is ignored.
 # You can also use a comment to "disable" or comment out a piece of code:
 # puts "This won't run"
 
-Puts "This will run"
+puts "This will run"
 puts "Tester"
