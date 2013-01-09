5e4f35924113dfbd126f70ecc4308829c9be3ccf
bskelty <brad@cheapcycleparts.com>
2013-01-09 10:36:53 -0500

    changes made in ex3

--- ex3.rb
+++ ex3.rb
@@ -11,8 +11,8 @@ puts "Is it true that 3 + 2 < 5 - 7?"
 
 puts 3 + 2 < 5 -7
 
-puts "What is 3 + 2?" 3 + 2
-puts "What is 5 - 7?" 5 - 7
+puts "What is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
 
 puts "Oh that's why it's false"
 
