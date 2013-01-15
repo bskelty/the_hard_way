01ec3a755a941a067508fefa4f54994de770e870
Brad Kelty <brad@cheapcycleparts.com>
2013-01-14 20:51:13 -0500

    ex13

---
+++ ex13.rb
@@ -0,0 +1,6 @@
+first, second, third = ARGV
+
+puts "The script is called: #{$0}"
+puts "Your firat variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
